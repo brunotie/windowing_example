7da6e2d12764833ee718d5d46d6f179797387f5a
brunotie <123190649+brunotie@users.noreply.github.com>
2023-01-21 12:17:08 +0100

    virtual dom basic implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
-import styles from "@/styles/Home.module.css";
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import { Ticket, TicketPage } from "./api/ticket";
 
 type Maybe<T> = T | undefined;
 
-function Row({ ticket }: { ticket: Maybe<Ticket> }) {
-    if (!ticket) return <div>loading</div>;
-    return <div>{ticket.subject}</div>;
+function Row({
+    ticket,
+    style,
+}: {
+    ticket: Maybe<Ticket>;
+    style?: CSSProperties;
+}) {
+    if (!ticket) return <div style={style}>loading</div>;
+    return <div style={style}>{ticket.subject}</div>;
 }
 
 export default function Home() {
     const [ticketCount, setTicketCount] = useState<number>();
     const [tickets, setTickets] = useState<Maybe<Ticket>[]>();
+    const [scrollPosition, setScrollPosition] = useState<number>(0);
+    const ROW_HEIGHT = 40;
 
     async function fetchTickets(page: number) {
         const res: TicketPage = await fetch(`/api/ticket?page=${page}`).then(
@@ -22,8 +29,6 @@ export default function Home() {
             ? tickets
             : new Array(res.count).fill(undefined);
         ticket_results.splice(page * 100, res.data.length, ...res.data);
-        console.log(ticket_results);
-        console.log(res.data);
         setTickets(ticket_results);
     }
 
@@ -32,8 +37,37 @@ export default function Home() {
     }, []);
 
     return (
-        <main className={styles.main}>
-            <div>{tickets && tickets.map((d,i) => <Row key={i} ticket={d} />)}</div>
-        </main>
+        <div
+            style={{ height: "500px", overflow: "auto", display: "flex" }}
+            onScroll={(e) => {
+                const target = e.target as HTMLDivElement;
+                setScrollPosition(target.scrollTop);
+            }}
+        >
+            <div
+                style={{
+                    height: (ticketCount ?? 0) * ROW_HEIGHT,
+                    width: "100%",
+                    position: "relative",
+                }}
+            >
+                {tickets &&
+                    tickets.map((d, i) =>
+                        i * ROW_HEIGHT > scrollPosition - 100 &&
+                        scrollPosition + window.innerHeight > i * ROW_HEIGHT ? (
+                            <Row
+                                key={i}
+                                ticket={d}
+                                style={{
+                                    position: "absolute",
+                                    top: i * ROW_HEIGHT,
+                                }}
+                            />
+                        ) : (
+                            <></>
+                        )
+                    )}
+            </div>
+        </div>
     );
 }
